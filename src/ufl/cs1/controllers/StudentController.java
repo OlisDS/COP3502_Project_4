1df453972c7f165b9bd17acf96f4f97be064f471
joeylu38 <joeylu@ufl.edu>
2017-12-04 21:42:06 -0500

    Revert "Revert "Weird stuff popped up and I deleted it""
    This reverts commit c732aa5a188384d01a321c10eb332a1a99abb16b.

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -5,12 +5,12 @@ import game.models.Actor;
 import game.models.Defender;
 import game.models.Game;
 import game.models.Node;
-<<<<<<< HEAD
+
 import game.models.Maze;
 import game.models.Attacker;
-=======
 
->>>>>>> b597e04ad28367c60e5567c074abe0965d152137
+
+
 import java.util.List;
 
 public final class StudentController implements DefenderController
@@ -45,10 +45,6 @@ public final class StudentController implements DefenderController
 	 */
 
 	public int ghost1(){
-<<<<<<< HEAD
-
-=======
->>>>>>> b597e04ad28367c60e5567c074abe0965d152137
 	    /* FIXME, blank method for now.
 	    Intercepter-kun work off premise IF it can get between pacman and the pill he's going to get between the two on
 	    pacman's current/predicted path (shortest), the best way of doing this would be to figure out the shortest path
@@ -56,58 +52,34 @@ public final class StudentController implements DefenderController
 	    ELSE should wait at safe distance (out of pacman's reach for when he gets the pill)
 	     */
 
-<<<<<<< HEAD
 		return 0;
 	}
 
-	public int ghost2(){
-=======
-        return 0;
-    }
-
     public int ghost2(){
->>>>>>> b597e04ad28367c60e5567c074abe0965d152137
 	    /* FIXME blank method for now.
 	    Stalker-kun should follow at a safe distance, if pacman gets near the pill and intercepter isn't on intercept path
 	    then should stay JUST out of range and then trigger pacman eating the pill by coming in range then running away
 	     */
 
-<<<<<<< HEAD
 		return 0;
 	}
 
-	public int ghost3(){
-=======
-        return 0;
-    }
 
     public int ghost3(){
->>>>>>> b597e04ad28367c60e5567c074abe0965d152137
         /* FIXME blank method for now
         Blocker-chan should block off paths that would result in pacman getting superpill position, path blocked
         should not be already covered by another ghost, determine based on last nodes in path
          */
 
-<<<<<<< HEAD
 		return 0;
 	}
 
-	public int ghost4(){
-=======
-        return 0;
-    }
-
     public int ghost4(){
->>>>>>> b597e04ad28367c60e5567c074abe0965d152137
         /* FIXME blank method
         Goalie san should be the furthest away ghost from doing anything, job will be to camp a power pill elsewhere
         This is a means of blocking off future paths and decisions by pacman
          */
-<<<<<<< HEAD
 		return 0;
 	}
-=======
-        return 0;
-    }
->>>>>>> b597e04ad28367c60e5567c074abe0965d152137
+
 }
